df68087edb1a94b24f5fbef39e19453b3109a5e9
CBedoy <carlos.bedoy@gmail.com>
2014-03-05 19:54:13 -0600

    Add IParser protocol

--- methodgravity/src/main/java/com/cbedoy/methodgravity/interfaces/IParser.java
+++ methodgravity/src/main/java/com/cbedoy/methodgravity/interfaces/IParser.java
@@ -1,10 +1,12 @@
 package com.cbedoy.methodgravity.interfaces;
 
+import org.json.JSONArray;
+
 import java.util.ArrayList;
 
 /**
  * Created by Carlos on 5/03/14.
  */
 public interface IParser {
-    public ArrayList<IModel> getDataParsed();
+    public ArrayList<IModel> getDataParsed(JSONArray array);
 }
