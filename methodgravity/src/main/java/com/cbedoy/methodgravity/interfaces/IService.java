d0547f1055b5deea174a23928c0ea2295b960ef4
CBedoy <carlos.bedoy@gmail.com>
2014-03-06 19:31:58 -0600

    Make service and IService for get response from server

---
+++ methodgravity/src/main/java/com/cbedoy/methodgravity/interfaces/IService.java
@@ -0,0 +1,11 @@
+package com.cbedoy.methodgravity.interfaces;
+
+import com.cbedoy.methodgravity.libs.CBRESTClient;
+
+/**
+ * Created by Carlos on 6/03/14.
+ */
+public interface IService {
+
+    public String runSerive(CBRESTClient request);
+}
--- methodgravity/src/main/java/com/cbedoy/methodgravity/services/ItemLocationService.java
+++ methodgravity/src/main/java/com/cbedoy/methodgravity/services/ItemLocationService.java
@@ -4,20 +4,25 @@ import android.app.Activity;
 import android.app.Application;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 
 import com.cbedoy.methodgravity.interfaces.IModel;
+import com.cbedoy.methodgravity.interfaces.IService;
 import com.cbedoy.methodgravity.libs.CBRESTClient;
 import com.cbedoy.methodgravity.models.LocationModel;
+
+import org.json.JSONArray;
 
 /**
  * Created by Carlos on 5/03/14.
  */
-public class ItemLocationService extends AsyncTask<String, Integer,String> {
+public class ItemLocationService extends AsyncTask<String, Integer,String> implements IService{
 
     private ProgressDialog progressDialog;
     private Context context;
     private IModel model;
+    private  CBRESTClient rest;
     public ItemLocationService(Context context, IModel data){
         this.context = context;
         this.model = model;
@@ -34,8 +39,20 @@ public class ItemLocationService extends AsyncTask<String, Integer,String> {
         CBRESTClient rest = new CBRESTClient("http://api.openweathermap.org/data/2.5/weather");
         LocationModel locationModel = (LocationModel)model;
         rest.AddParam("q",  locationModel.getNombre());
-        
-        return "";
+        return runSerive(rest);
+    }
+
+    @Override
+    public String runSerive(CBRESTClient request) {
+
+        try {
+            request.Execute(CBRESTClient.RequestMethod.GET);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return (request.getResponse() != null)?request.getResponse():"";
+
     }
 
     //http://api.openweathermap.org/data/2.5/weather?q=Aguascalientes
@@ -43,5 +60,15 @@ public class ItemLocationService extends AsyncTask<String, Integer,String> {
     @Override
     protected void onPostExecute(String result){
         this.progressDialog.dismiss();
+        try{
+            JSONArray jsonArray = new JSONArray(result);
+
+        }catch (Exception e){
+
+        }
+
     }
+
+
+
 }
