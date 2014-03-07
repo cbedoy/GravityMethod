90aaa79d069fd94364637f9da87da3bb853e87c7
CBedoy <carlos.bedoy@gmail.com>
2014-03-06 19:39:25 -0600

    Make some data for àrser

--- methodgravity/src/main/java/com/cbedoy/methodgravity/parsers/ItemLocationParser.java
+++ methodgravity/src/main/java/com/cbedoy/methodgravity/parsers/ItemLocationParser.java
@@ -2,8 +2,10 @@ package com.cbedoy.methodgravity.parsers;
 
 import com.cbedoy.methodgravity.interfaces.IModel;
 import com.cbedoy.methodgravity.interfaces.IParser;
+import com.cbedoy.methodgravity.models.ResponseModel;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -15,6 +17,17 @@ public class ItemLocationParser implements IParser {
     @Override
     public ArrayList<IModel> getDataParsed(JSONArray array) {
         ArrayList<IModel> data = new ArrayList<IModel>();
+
+        ResponseModel model = new ResponseModel();
+        try{
+            JSONObject coord = array.getJSONObject(1);
+            model.setLatitud((float)coord.getDouble("lat"));
+            model.setLongitud((float)coord.getDouble("lon"));
+
+
+        }catch (Exception e){
+
+        }
 
         return data;
     }
